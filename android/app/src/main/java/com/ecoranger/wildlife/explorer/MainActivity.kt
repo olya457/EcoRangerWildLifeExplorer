504c16aea068ce4d53e6114fb241ff5195c556e7
olya <olya0665643972@gmail.com>
2026-05-27 10:35:08 +0300

    init

--- android/app/src/main/java/com/ecoranger/wildlife/explorer/MainActivity.kt
+++ android/app/src/main/java/com/ecoranger/wildlife/explorer/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ecorangerwildlifeexplorer
+package com.ecoranger.wildlife.explorer
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/ecoranger/wildlife/explorer/MainApplication.kt
+++ android/app/src/main/java/com/ecoranger/wildlife/explorer/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.ecorangerwildlifeexplorer
+package com.ecoranger.wildlife.explorer
 
 import android.app.Application
 import com.facebook.react.PackageList
